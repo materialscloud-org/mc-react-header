0a6ab1cb6ecf28ad94480cb49cc40fd9f226f81a
Kristjan Eimre <kristjaneimre@gmail.com>
2022-12-14 04:39:17 +0200

    small alignment fix

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 
 import MaterialsCloudHeader from "./MaterialsCloudHeader";
 
+import Logo from "./MaterialsCloudHeader/assets/mcloud_logo.png";
+
 function App() {
   return (
     <div className="App">
@@ -16,6 +18,7 @@ function App() {
         <h3>Test content</h3>
         <p>Test content</p>
       </MaterialsCloudHeader>
+      <a style={{fontSize: 0}}><img src={Logo} height="50" alt="MC logo" /></a>
     </div>
   );
 }
