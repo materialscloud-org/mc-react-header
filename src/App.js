6aaef1850c08cba2b5e368c71fad0c4e95ab8713
Kristjan Eimre <kristjaneimre@gmail.com>
2023-01-15 05:40:33 +0200

    responsive breadcrumbs

--- src/App.js
+++ src/App.js
@@ -2,23 +2,20 @@ import "./App.css";
 
 import MaterialsCloudHeader from "./MaterialsCloudHeader";
 
-import Logo from "./MaterialsCloudHeader/assets/mcloud_logo.png";
-
 function App() {
   return (
     <div className="App">
       <MaterialsCloudHeader
         activeSection={"discover"} // see the corresponding key in MaterialsCloudHeader/Navbar.js
         breadcrumbsPath={[
-          { name: "Work", link: "https://www.materialscloud.org/work" },
-          { name: "Tools", link: "https://www.materialscloud.org/work/tools" },
-          { name: "SeeK-path", link: null },
+          { name: "Discover", link: "https://www.materialscloud.org/discover" },
+          { name: "Materials Cloud three-dimensional crystals database", link: "https://www.materialscloud.org/discover/mc3d" },
+          { name: "test", link: null },
         ]}
       >
         <h3>Test content</h3>
         <p>Test content</p>
       </MaterialsCloudHeader>
-      <a style={{fontSize: 0}}><img src={Logo} height="50" alt="MC logo" /></a>
     </div>
   );
 }
